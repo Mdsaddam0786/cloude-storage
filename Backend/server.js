ff6e5505d216b54d1edf5b18da7d4d921a1d5041
Md Saddam <91770727+Mdsaddam0786@users.noreply.github.com>
2025-11-22 15:56:53 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -63,3 +63,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+app.get('/', (req, res) => {
+  res.send('✅ Backend is working!');
+});
