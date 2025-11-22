4ab64eacbaeb3de91aea342f8dc9e0fcb7b68abc
Md Saddam <91770727+Mdsaddam0786@users.noreply.github.com>
2025-11-22 12:55:33 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ match your backend
+  baseURL: 'https://cloude-storage-tan.vercel.app/', // ✅ match your backend
 });
 
 api.interceptors.request.use((config) => {
