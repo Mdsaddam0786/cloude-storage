8f4ef32590d712d9a0052491ce4044beb28bc435
Md Saddam <91770727+Mdsaddam0786@users.noreply.github.com>
2025-11-22 13:49:22 +0530

    Remove trailing slash from baseURL in api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://cloude-storage.onrender.com/', // ✅ match your backend
+  baseURL: 'https://cloude-storage.onrender.com', // ✅ match your backend
+  
 });
 
 api.interceptors.request.use((config) => {
